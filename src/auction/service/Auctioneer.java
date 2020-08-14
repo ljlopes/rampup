656c51f0b2c23009feba5a7344ab34bc492e1865
ljlopes <ljlopes@daitan.com>
2020-08-14 11:25:22 -0300

    Add Bids average value tests.

--- src/auction/service/Auctioneer.java
+++ src/auction/service/Auctioneer.java
@@ -7,12 +7,22 @@ public class Auctioneer {
 
     private double highestOfAll = Double.NEGATIVE_INFINITY;
     private double lowestOfAll = Double.POSITIVE_INFINITY;
+    private double average = 0;
 
     public void evaluate(Auction auction){
+
+        double total = 0;
         for(Bid bid : auction.getBids()){
             if(bid.getValue() > highestOfAll)highestOfAll = bid.getValue();
             if(bid.getValue() < lowestOfAll) lowestOfAll = bid.getValue();
+            total += bid.getValue();
         }
+        if(total == 0) {
+            average = 0;
+            return;
+        }
+
+        average = total/auction.getBids().size();
     }
 
     public double getHighestBid() {
@@ -21,5 +31,9 @@ public class Auctioneer {
 
     public double getLowestBid() {
         return lowestOfAll;
+    }
+
+    public double getAverage() {
+        return average;
     }
 }
--- src/auction/test/AuctioneerTest.java
+++ src/auction/test/AuctioneerTest.java
@@ -32,4 +32,38 @@ public class AuctioneerTest {
         assertEquals(lowestExpected, auctioneer.getLowestBid(), 0.00001);
 
     }
+
+    @Test
+    public void shouldCalculateAverageValueOfBids(){
+
+        User jhon = new User("Jhon");
+        User mark = new User("Mark");
+        User ana = new User("Ana");
+
+        Auction auction = new Auction("Headphone Bluetooth JBL");
+
+        auction.propose(new Bid(jhon, 350.00));
+        auction.propose(new Bid(mark, 100.00));
+        auction.propose(new Bid(ana, 150.00));
+
+        Auctioneer auctioneer = new Auctioneer();
+
+        auctioneer.evaluate(auction);
+
+        assertEquals( 200.00, auctioneer.getAverage(), 0.0001);
+    }
+
+    @Test
+    public void shouldCalculateAverageValueOfZeroBids(){
+
+        User jhon = new User("Jhon");
+
+        Auction auction = new Auction("Iphone 11");
+
+        Auctioneer auctioneer = new Auctioneer();
+
+        auctioneer.evaluate(auction);
+
+        assertEquals(0, auctioneer.getAverage(), 0.0001);
+    }
 }
