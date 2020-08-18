7bd2e5ec7490b065e40c37f2c1a8576e8e8d482f
ljlopes <ljlopes@daitan.com>
2020-08-18 16:59:43 -0300

    Add project with Maven.

---
+++ produtos/src/main/java/rampup/maven/App.java
@@ -0,0 +1,13 @@
+package rampup.maven;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ produtos/src/main/java/rampup/maven/Produto.java
@@ -0,0 +1,12 @@
+package rampup.maven;
+
+public class Produto{
+
+    private final String nome;
+    private final double preço;
+
+    public Produto(String nome, double preço) {
+        this.nome = nome;
+        this.preço = preço;
+    }
+}
